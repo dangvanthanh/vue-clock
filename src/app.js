5b52e5ea83568b7a1f3e47f03617462a09c36982
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-01-11 21:32:52 +0700

    Add alias @

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Clock from './components/Clock.vue'
+import Vue from 'vue';
+import Clock from '@/components/Clock.vue';
 
 const app = new Vue({
   el: '#app',
   components: {
-    Clock
-  }
-})
+    Clock,
+  },
+});
