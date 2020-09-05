53fcd6288c6698a92cf746c730ae4b781dc7cb84
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-09-05 20:00:31 +0700

    Remove rollup watch

--- build/rollup.config.browsers.js
+++ build/rollup.config.browsers.js
@@ -3,8 +3,6 @@ import esbuild from 'rollup-plugin-esbuild';
 import base from './rollup.config.base';
 import pkg from '../package.json';
 
-const production = !process.env.ROLLUP_WATCH;
-
 const config = Object.assign({}, base, {
   output: {
     exports: 'named',
@@ -15,6 +13,6 @@ const config = Object.assign({}, base, {
 });
 
 config.plugins.push(vue());
-config.plugins.push(esbuild({ minify: production }));
+config.plugins.push(esbuild({ minify: true }));
 
 export default config;
